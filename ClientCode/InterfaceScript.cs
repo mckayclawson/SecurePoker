43491df8850fa4ffa2f02a236747d7aec5033884
Zak Terry <zackterry@gmail.com>
2015-03-13 12:26:51 -0400

    Client Code

---
+++ ClientCode/InterfaceScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterfaceScript : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)) {
+				if(hit.transform.parent != null) {
+					if(hit.transform.parent.gameObject.name == "Card") {
+						CardScript cs = hit.transform.parent.gameObject.GetComponent<CardScript>();
+						cs.flip();
+					}
+				}
+			}
+		}
+	}
+}
---
+++ ClientCode/ServerCommunicator.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+public class ServerCommunicator : MonoBehaviour {
+
+	string postUrl = "http://localhost:5000";
+
+	bool waiting = false;
+
+	public void sendSignInRequest(string username, string password) {
+		JObject signInObject = new JObject();
+		signInObject.Add ("message_type", "sign_in");
+		signInObject.Add ("username", username);
+		signInObject.Add ("password", password);
+
+		WWWForm form = new WWWForm();
+		form.AddField("json", signInObject.ToString());
+
+		Debug.Log(signInObject.ToString());
+
+		WWW www = new WWW(postUrl, form);
+
+		waiting = true;
+
+		StartCoroutine(WaitForRequest(www));
+	}
+
+	public void sendRegisterRequest(string username, string password) {
+		JObject regObject = new JObject();
+		regObject.Add ("message_type", "register");
+		regObject.Add ("username", username);
+		regObject.Add ("password", password);
+		Debug.Log (regObject.ToString());
+
+		WWWForm form = new WWWForm();
+		form.AddField("json", regObject.ToString());
+		
+		WWW www = new WWW(postUrl, form);
+		
+		waiting = true;
+		
+		StartCoroutine(WaitForRequest(www));
+	}
+
+	IEnumerator WaitForRequest(WWW www) {
+		yield return www;
+		Debug.Log("sending");
+		// check for errors
+		if (www.error == null) {
+			Debug.Log("WWW Ok!: " + www.text);
+			waiting = false;
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}    
+	}
+}
